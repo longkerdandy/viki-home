ee5c48fe8864581a6d10f05bd09cf7dda0f2eb34
LongkerDandy <longkerdandy@gmail.com>
2018-04-13 10:46:02 +0800

    Add missing source code file.

---
+++ viki-home-hap/src/main/java/com/github/longkerdandy/viki/home/hap/model/property/Unit.java
@@ -0,0 +1,35 @@
+package com.github.longkerdandy.viki.home.hap.model.property;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Unit of the Characteristic's Value
+ */
+public enum Unit {
+
+  CELSIUS("celsius"),
+  PERCENTAGE("percentage"),
+  ARCDEGREES("arcdegrees"),
+  lux("lux"),
+  SECONDS("seconds");
+
+  private final String value;
+
+  Unit(String value) {
+    this.value = value;
+  }
+
+  public static Unit fromValue(String value) {
+    for (Unit u : values()) {
+      if (u.value.equals(value)) {
+        return u;
+      }
+    }
+    throw new IllegalArgumentException("invalid unit: " + value);
+  }
+
+  @JsonValue
+  public String value() {
+    return this.value;
+  }
+}
